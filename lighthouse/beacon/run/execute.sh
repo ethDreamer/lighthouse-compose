ecae5270fd0af98631b7bbacb0ce48f1075341d5
Mark Mackey <mark@sigmaprime.io>
2026-05-05 19:11:38 -0500

    set wan ip in lighthouse

--- lighthouse/beacon/run/execute.sh
+++ lighthouse/beacon/run/execute.sh
@@ -14,7 +14,10 @@ if [ "$MEVBOOST_ENABLED" = "true" ]; then
     BUILDER_ARG="--builder=http://mev-boost:8560"
 fi
 
+export WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
 echo "******************** STARTING LIGHTHOUSE BEACON NODE ********************"
+echo "WAN IP: $WANIP"
 
 exec lighthouse \
     --log-color \
@@ -27,6 +30,7 @@ exec lighthouse \
     --discovery-port $CONSENSUS_DISC \
     --port $CONSENSUS_DISC \
     --http-address=0.0.0.0 \
+    --enr-address=$WANIP \
     $(printf '%s' "$METRICS_ARG") \
     --execution-jwt=/shared/jwt.secret \
     --execution-endpoint=$EE_TARGET \
